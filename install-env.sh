543dfcb4a5616bd19ccd26f09a9a5867310a56b9
langenoir <Ln1878@langenoirs-MacBook-Pro.local>
2015-10-02 15:43:01 -0500

    added two files to the LocalGit/Environment-setup index.html for practice install-ene.sh as shellscript to set up apache webserver and git repo 1st attempt
    2015-10-02-15:45:31
    @PS3001-718

---
+++ install-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/LangenoirNoir1878/Environment-setup.git
+
+mv ./Environment-setup/images /var/www/html/images
+mv ./Environment-setup/index.html /var/www/html
+
+echo "Hi, install-env.sh has been called!" > /tmp/hello.txt
+
+
