0eb30b596072c920b4d5e0deb833d38919b5625d
langenoir <Ln1878@dhcp226.merusouth115.iit.edu>
2015-10-25 13:24:59 -0500

    changed the directory of the clone repo

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
+sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
 
 sudo curl -sS http://getcomposer.org/installer | php
 sudo php composer.phar require aws/aws-sdk-php
 
 sudo git clone https://github.com/Langenoir1878/Application-setup.git
 
-mv ./Environment-setup/images /var/www/html/images
-mv ./Environment-setup/index.html /var/www/html
+mv ./Application-setup/images /var/www/html/images
+mv ./Application-setup/index.html /var/www/html
 
-echo "Hi, install-env.sh has been called!" > /tmp/hello.txt
+echo "Hi, install-webserver.sh has been called!" > /tmp/hello.txt
 
