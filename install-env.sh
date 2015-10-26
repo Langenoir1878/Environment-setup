07cae5b6b45fae9ba41d515dc44cf2488c0a91d7
langenoir <Ln1878@dhcp226.merusouth115.iit.edu>
2015-10-26 09:30:34 -0500

    previous commit message was belong to this commit...selected a wrong folder to commit

--- install-env.sh
+++ install-env.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/Langenoir1878/Environment-setup.git
 
 mv ./Environment-setup/images /var/www/html/images
 mv ./Environment-setup/index.html /var/www/html
+mv ./Application-setup/images /var/www/html/images
+mv ./Application-setup/*.php /var/www/html
+
+
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+
+# ref Repo:https://github.com/jhajek/itmo-544-444-env/blob/master/install-webserver.sh
+
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
+
 
 echo "Hi, install-env.sh has been called!" > /tmp/hello.txt
 
--- install-webserver.sh
+++ install-webserver.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+# Repo ref: https://github.com/jhajek/itmo-544-444-env/blob/master/install-webserver.sh
 
+sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
 
-sudo curl -sS http://getcomposer.org/installer | php
-sudo php composer.phar require aws/aws-sdk-php
+
 
 sudo git clone https://github.com/Langenoir1878/Application-setup.git
 
 mv ./Application-setup/images /var/www/html/images
 mv ./Application-setup/index.html /var/www/html
+mv ./Application-setup/*.php /var/www/html
+
+
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
+
 
 echo "Hi, install-webserver.sh has been called!" > /tmp/hello.txt
 
