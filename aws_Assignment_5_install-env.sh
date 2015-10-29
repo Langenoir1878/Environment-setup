f6c74b35f2a3f04fa0a74f25eb7ddcd67478e2ba
langenoir <Ln1878@dhcp84.merusouth116.iit.edu>
2015-10-29 00:56:12 -0500

    added git clone application

--- aws_Assignment_5_install-env.sh
+++ aws_Assignment_5_install-env.sh
@@ -4,9 +4,10 @@
 # installing environment
 # Oct 28th
 sudo apt-get update -y
-sudo apt-get install -y apache2 git 
+sudo apt-get install -y apache2 git php5 curl php5-curl 
 
 git clone https://github.com/Langenoir1878/Environment-setup.git
+git clone https://github.com/Langenoir1878/Application-setup.git
 
 mv ./Environment-setup/images /var/www/html/images
 mv ./Application-setup/*.html /var/www/html
