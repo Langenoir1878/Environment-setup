30964a2d77cd138f830c91c7b4ff977decb7090e
langenoir <Ln1878@dhcp84.merusouth116.iit.edu>
2015-10-28 23:13:00 -0500

    added teh install-env.sh for assignment 5

---
+++ aws_Assignment_5_install-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Assignment 5 redo:
+# Yiming Zhang 
+# installing environment
+# Oct 28th
+sudo apt-get update -y
+sudo apt-get install -y apache2 git 
+
+git clone https://github.com/Langenoir1878/Environment-setup.git
+
+mv ./Environment-setup/images /var/www/html/images
+mv ./Environment-setup/index.html /var/www/html
+mv ./Application-setup/images /var/www/html/images
+mv ./Application-setup/*.php /var/www/html
+
+
+
+
+echo "Launching environment has been successfully installed for Assignment 5 on " . time > /tmp/hello.txt
